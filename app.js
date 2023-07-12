653832383f4dd2ff1e29c4135133753e65292bf9
ducdv <ducdv@dlsinc.com>
2023-07-12 11:04:13 +0700

    update ci cd

--- app.js
+++ app.js
@@ -39,20 +39,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.post('/', (req, res) => {
-  res.send('Got a POST request')
-})
-
-app.put('/user', (req, res) => {
-  res.send('Got a PUT request at /user')
-})
-
-app.delete('/user', (req, res) => {
-  res.send('Got a DELETE request at /user')
-})
-
 module.exports = app;
